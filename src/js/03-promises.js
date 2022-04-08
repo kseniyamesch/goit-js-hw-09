71c52a020935989d1ef07d8878f120287ed21a28
Kseniia Malyshenko <kseniyamesch0101@gmail.com>
2022-04-08 12:53:47 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,25 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  delay: document.querySelector('input[name="delay"]'),
-  step: document.querySelector('input[name="step"]'),
-  amount: document.querySelector('input[name="amount"]'),
+  // delay: document.querySelector('input[name="delay"]'),
+  // step: document.querySelector('input[name="step"]'),
+  // amount: document.querySelector('input[name="amount"]'),
   form: document.querySelector('.form')
 };
 refs.form.addEventListener('submit', onFormSubmit);
  
-console.log(typeof(refs.delay.value))
+// console.log(typeof(refs.delay.value))
 
 
   function onFormSubmit (evt) {
     evt.preventDefault();
-  const delay = Number(refs.delay.value);
-  const step = Number(refs.step.value);
-  const amount = Number(refs.amount.value);
+  // const delay = Number(refs.delay.value);
+  // const step = Number(refs.step.value);
+  // const amount = Number(refs.amount.value);
+const delay = Number(evt.currentTarget.elements[0].value);
+const step = Number(evt.currentTarget.elements[1].value);
+const amount = Number(evt.currentTarget.elements[2].value);
+
   let calculatedDelay = delay;
   for (let i = 1; i <= amount; i += 1) {
     createPromise (i, calculatedDelay).then(({ position, delay }) => {
